60c12a09a5adcefbcd1c64249cbed398f19d8635
jandro29 <ascolanr@gmail.com>
2025-06-20 02:23:12 -0500

    login

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
-  providers: [ provideRouter(routes) ]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule)  // importante para Angular Material
+  ]
 });
