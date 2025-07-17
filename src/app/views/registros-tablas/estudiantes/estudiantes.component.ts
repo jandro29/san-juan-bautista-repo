ec232a22fa5baabea804430b5223f1e294dc5c18
Jose Carlos Colan <ascolanr@gmail.com>
2025-07-17 16:33:37 -0500

    .

--- src/app/views/registros-tablas/estudiantes/estudiantes.component.ts
+++ src/app/views/registros-tablas/estudiantes/estudiantes.component.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 const supabaseUrl = "https://fgfmtlvmpmiudjbufrjb.supabase.co";
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnZm10bHZtcG1pdWRqYnVmcmpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzOTk1MTUsImV4cCI6MjA2NTk3NTUxNX0.RFIiNRunac0E1GhUwE6VKRpTNksW1y-s62GIY3DzGHA';
@@ -24,7 +25,8 @@ const supabase = createClient(supabaseUrl, supabaseKey);
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatButtonModule
+    MatButtonModule,
+    CommonModule
   ]
 })
 export class EstudiantesComponent {
