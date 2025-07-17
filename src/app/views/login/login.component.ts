2bd955d69c56c4600a7018d2872c1926d10deb93
Jose Carlos Colan <ascolanr@gmail.com>
2025-07-17 17:52:20 -0500

    .

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -7,7 +7,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   form: FormGroup;
